38aa71ff4203ffbff3b1613ffb35b9dd38fc04fe
assassin <robinmulder1205@outlook.com>
2025-05-07 23:36:23 +0900

    Updated the my portfolio

--- src/components/navbar/NavLinks.js
+++ src/components/navbar/NavLinks.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaLinkedin, FaTwitter} from "react-icons/fa"
+import { FaTelegram, FaDiscord} from "react-icons/fa"
 import {BiEnvelope} from "react-icons/bi"
 import {BsGithub} from "react-icons/bs"
 
 const NavLinks = ({handleNav}) => {
   return (
     <ul className='nav-links'>
-        <li onClick={handleNav} ><Link  to="//twitter.com/_pappy_jay_" target='_blank' className='nav-link'><FaTwitter /></Link></li>
-        <li onClick={handleNav} ><Link  to="//www.linkedin.com/in/pappyjay" target='_blank' className='nav-link'><FaLinkedin /></Link></li>
-        <li onClick={handleNav} ><Link  to="//github.com/Pappyjay23" target='_blank' className='nav-link'><BsGithub /></Link></li>
-        <li onClick={handleNav} ><a  href="mailto:pjinadu02@gmail.com" target='_blank' className='nav-link' rel="noreferrer"><BiEnvelope /></a></li>
+        <li onClick={handleNav} ><Link  to="//" target='_blank' className='nav-link'><FaDiscord /></Link></li>
+        <li onClick={handleNav} ><Link  to="//" target='_blank' className='nav-link'><FaTelegram /></Link></li>
+        <li onClick={handleNav} ><Link  to="//github.com/robi0918" target='_blank' className='nav-link'><BsGithub /></Link></li>
+        <li onClick={handleNav} ><a  href="goldclock0918@gmail.com" target='_blank' className='nav-link' rel="noreferrer"><BiEnvelope /></a></li>
   </ul>
   )
 };
